0920700f17a27449afbbbaddd6519a7cb6aa8b16
Tulio Roberto <tuliorobert2@gmail.com>
2022-01-30 01:38:04 -0300

    Endpoint adjust /categorias/{id}

--- src/main/java/com/tulioroberto/cursomc/services/CategoriaService.java
+++ src/main/java/com/tulioroberto/cursomc/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.tulioroberto.cursomc.domain.Categoria;
 import com.tulioroberto.cursomc.repositories.CategoriaRepository;
+import com.tulioroberto.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
-	public Categoria find(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+
+	public Categoria find(Integer id) { 
+		 Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException( 
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
+		}
 }
