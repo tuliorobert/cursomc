65b9fd86ac5c76b521971447845d0ba5a451d050
Tulio <tuliorobert2@gmail.com>
2022-02-14 13:36:44 -0300

    Using 'JsonIgnore' only

--- src/main/java/com/tulioroberto/cursomc/domain/Endereco.java
+++ src/main/java/com/tulioroberto/cursomc/domain/Endereco.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Endereco implements Serializable {
@@ -21,7 +21,7 @@ public class Endereco implements Serializable {
 	private Integer id;
 	private String logradouro, numero, complemento, bairro, cep;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
